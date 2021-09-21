6b3a29782d4ae5640cdb3db2e3d38fbbebfd8239
Yaoting Jiang <yaotingjiang@gmail.com>
2021-09-20 21:52:15 -0700

    done

--- lms.py
+++ lms.py
@@ -1,4 +1,17 @@
-from main import Book, Borrower
+from main import Book, Borrower, add_books, add_borrowers
+from mongoengine import *
+from tabulate import tabulate
+
+
+default_db_name = 'app-yaotingj'
+
+def reset_db():
+    disconnect(alias="default")
+    newdb_connection = connect(default_db_name)
+    newdb_connection.drop_database(default_db_name)
+    add_books()
+    add_borrowers()
+
 
 def get_borrower_name(borrowerID):
     for borrower in Borrower.objects:
@@ -14,62 +27,65 @@ def checkout_book(borrowerID, bookID):
     for b in Book.objects:
         if b.book_id == bookID and b.checked_out == "Y" and b.borrower_id != borrowerID:
             print(get_book_name(bookID), "is already checked out by someone")
-        elif b.book_id == bookID and b.checked_out == "":
+        elif b.book_id == bookID and b.checked_out == "N":
             b.checked_out = "Y"
             b.borrower_id=borrowerID
             b.borrower_name=get_borrower_name(borrowerID)
-            #TODO update DB
             b.save()
-    print(get_borrower_name(borrowerID), 'has checked out ', get_book_name(bookID))
+    print(get_borrower_name(borrowerID), 'has checked out', get_book_name(bookID))
 
 def return_book(borrowerID, bookID):
     for b in Book.objects:
-        if b.book_id == bookID and b.checked_out == "" and b.borrower_id != borrowerID :
-            # TODO
-            # What should be the initial value of checked_out
+        if b.book_id == bookID and b.checked_out == "N" and b.borrower_id != borrowerID :
             print(get_borrower_name(b.borrower_id), 'has not currently checked out', get_book_name(bookID))
         elif b.book_id == bookID and b.checked_out == "Y" and b.borrower_id == borrowerID:
-            b.checked_out =""
-            b.borrower_id=""
-            b.borrower_name=""
-            #TODO update DB
+            b.checked_out = "N"
+            b.borrower_id = ""
+            b.borrower_name = ""
             b.save()
             print(get_borrower_name(borrowerID), 'has returned ', get_book_name(bookID))
 
-def reset_db():
-    for book in Book.objects:
-        book.checked_out = ""
-        book.borrower_id = ""
-        book.borrower_name = ""
-        #TODO update DB
-        book.save()
+def get_books():
+    book_list = []
+    for b in Book.objects:
+        book_list.append([f'"{b.book_id}"', f'"{b.title}"', f'"{b.author}"', f'"{b.checked_out}"',
+                          f'"{b.borrower_id}"', f'"{b.borrower_name}"'])
+    print(tabulate(book_list,
+                    headers=['book_id', 'title','author', 'checked_out', 'borrower_id', 'borrower_name']))
+
+
 
 if __name__ == "__main__":
+
+    newdb = input('Enter a database name: ')
+    if newdb == "":
+        add_books()
+        add_borrowers()
+        print(default_db_name, 'is connected')
+    else:
+        default_db_name = newdb
 
     while True:
         action = input('Enter a command: ')
 
-        if action == "checkout a book" or action == "return a book":
+        if action == "checkout" or action == "return":
             borrower_id = input("Enter Borrower Id: ")
             book_id = input("Enter Book Id: ")
 
             is_bookid_valid = any(book.book_id == book_id for book in Book.objects)
             is_borrowerid_valid = any(borrower.borrower_id == borrower_id for borrower in Borrower.objects)
 
-            print('test', is_bookid_valid)
-            print('test 2', is_borrowerid_valid)
-
             # checkout a book
-            if action == "checkout a book":
+            if action == "checkout":
                 if not is_bookid_valid:
-                    print('Book with ID ', book_id, ' does not exist')
+                    print('Book with ID ', book_id, 'does not exist')
                 elif not is_borrowerid_valid:
-                    print('Borrower with ID', borrower_id, ' does not exist')
+                    print('Borrower with ID', borrower_id, 'does not exist')
                 else:
                     checkout_book(borrower_id, book_id)
 
             # return a book
-            if action == "return a book":
+            if action == "return":
                 if not is_bookid_valid:
                     print('Book with ID ', book_id, ' does not exist')
                 elif not is_borrowerid_valid:
@@ -84,8 +100,7 @@ if __name__ == "__main__":
 
         # print books
         elif action == "books":
-            for b in Book.objects:
-                print(b.book_id,",", b.title,",", b.author,",", b.checked_out,",", b.borrower_id,",", b.borrower_name)
+            get_books()
 
         # exit
         elif action == "exit":
@@ -94,5 +109,5 @@ if __name__ == "__main__":
 
         # invalid request
         else:
-            print("invalid request, please enter a valid request")
+            print("invalid command, please enter a valid command")
             continue
